cd89616e2b0500b4b90766db248fb6bdc50743b4
typhonrt <support@typhonjs.io>
2023-04-29 15:11:40 -0700

    Checked in dist & types to be able to install package from Github for testing.

---
+++ dist/src/index-modern.js
@@ -0,0 +1,12 @@
+import { Mat2 } from './mat2.js';
+import { Mat2d } from './mat2d.js';
+import { Mat3 } from './mat3.js';
+import { Mat4 } from './mat4.js';
+import { Quat } from './quat.js';
+import { Quat2 } from './quat2.js';
+import { Vec2 } from './vec2.js';
+import { Vec3 } from './vec3.js';
+import { Vec4 } from './vec4.js';
+import { EnableSwizzles } from './swizzle.js';
+export { Mat2, Mat2d, Mat3, Mat4, Quat, Quat2, Vec2, Vec3, Vec4, EnableSwizzles };
+//# sourceMappingURL=index-modern.js.map
